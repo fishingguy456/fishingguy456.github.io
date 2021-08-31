b1b5c07576921694a4e3463106dea6956918d030
kevin qu <qukevin99@gmail.com>
2021-08-30 23:55:02 -0400

    formatting

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function toggleTheme() {
         light = false;
         document.documentElement.style.setProperty('--bg-colour', 'rgb(0, 0, 0)');
         document.documentElement.style.setProperty('--text-colour', 'rgb(255, 255, 255)');
-        document.getElementById("dark-mode").style.display = "none";//hide
-        document.getElementById("light-mode").style.display = "block";//show
+        document.getElementById("dark-mode").style.display = "none"; //hide
+        document.getElementById("light-mode").style.display = "block"; //show
     } else {
         light = true;
         document.documentElement.style.setProperty('--bg-colour', 'rgb(255, 255, 255)');
         document.documentElement.style.setProperty('--text-colour', 'rgb(0, 0, 0)');
-        document.getElementById("light-mode").style.display = "none";//hide
-        document.getElementById("dark-mode").style.display = "block";//show
+        document.getElementById("light-mode").style.display = "none"; //hide
+        document.getElementById("dark-mode").style.display = "block"; //show
     }
 }
