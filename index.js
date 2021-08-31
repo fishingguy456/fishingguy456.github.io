71df5dd09a0df13dd69b1e630de45de9600587e9
kevin qu <qukevin99@gmail.com>
2021-08-31 11:53:09 -0400

    store theme in cache

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-let light = false;
-function toggleTheme() {
-    if (light) {
-        light = false;
+let light;
+window.addEventListener('load', (event) => {
+    light = localStorage.getItem('light');
+    if (light == 'false') { //dark in browser cache
         document.documentElement.style.setProperty('--bg-colour', 'rgb(0, 0, 0)');
         document.documentElement.style.setProperty('--text-colour', 'rgb(255, 255, 255)');
-        document.getElementById("dark-mode").style.display = "none"; //hide
-        document.getElementById("light-mode").style.display = "block"; //show
-    } else {
-        light = true;
+    } else { //light in browser cache
         document.documentElement.style.setProperty('--bg-colour', 'rgb(255, 255, 255)');
         document.documentElement.style.setProperty('--text-colour', 'rgb(0, 0, 0)');
-        document.getElementById("light-mode").style.display = "none"; //hide
-        document.getElementById("dark-mode").style.display = "block"; //show
+        document.getElementById('light-mode').style.display = 'none'; //hide
+        document.getElementById('dark-mode').style.display = 'block'; //show
+    }
+});
+
+function toggleTheme() {
+    light = localStorage.getItem('light');
+    if (light == 'true') { //light to dark
+        localStorage.setItem('light', 'false');
+        document.documentElement.style.setProperty('--bg-colour', 'rgb(0, 0, 0)');
+        document.documentElement.style.setProperty('--text-colour', 'rgb(255, 255, 255)');
+        document.getElementById('dark-mode').style.display = 'none'; //hide
+        document.getElementById('light-mode').style.display = 'block'; //show
+    } else { //dark to light
+        localStorage.setItem('light', 'true');
+        document.documentElement.style.setProperty('--bg-colour', 'rgb(255, 255, 255)');
+        document.documentElement.style.setProperty('--text-colour', 'rgb(0, 0, 0)');
+        document.getElementById('light-mode').style.display = 'none'; //hide
+        document.getElementById('dark-mode').style.display = 'block'; //show
     }
 }
